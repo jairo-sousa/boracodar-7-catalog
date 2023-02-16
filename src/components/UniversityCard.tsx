0360ec3cfedab603aa486103a38bf95efe45fe89
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-02-16 15:22:18 -0300

    feat: create UniversityCard main structure

--- src/components/UniversityCard.tsx
+++ src/components/UniversityCard.tsx
@@ -1,5 +1,18 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Heading, Img, Text } from "@chakra-ui/react";
 
 export function UniversityCard() {
-	return <Flex>UniversityCard</Flex>;
+	return (
+		<Flex>
+			<Img />
+			<Heading as="h3">Title</Heading>
+			<Text>
+				Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+				sint.
+			</Text>
+			<Box>
+				<Img src="https://raw.githubusercontent.com/jairo-sousa/boracodar-7-catalog/2ead926aa93135bf69f858d47bdc08d1c1e80e8f/.github/magnifier.svg" />
+				<Text>City</Text>
+			</Box>
+		</Flex>
+	);
 }
