087b6e686fef1884e7df0c3d0a5c1e8d20ac1fe8
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-02-16 16:05:31 -0300

    feat: create universities data

---
+++ src/utils/universities.ts
@@ -0,0 +1,47 @@
+export const universities = [
+	{
+		university: "UNIVERSIDADE FEDERAL DA BAHIA",
+		state: "Bahia",
+		img: "https://raw.githubusercontent.com/jairo-sousa/boracodar-7-catalog/2ead926aa93135bf69f858d47bdc08d1c1e80e8f/.github/UFBA.jpg",
+	},
+	{
+		university: "UNIVERSIDADE FEDERAL DA FRONTEIRA DO SUL",
+		state: "Paraná",
+		img: "https://raw.githubusercontent.com/jairo-sousa/boracodar-7-catalog/2ead926aa93135bf69f858d47bdc08d1c1e80e8f/.github/UFFS.jpg",
+	},
+	{
+		university: "UNIVERSIDADE FEDERAL DA GRANDE DOURADOS",
+		state: "Mato Grosso do Sul",
+		img: "https://raw.githubusercontent.com/jairo-sousa/boracodar-7-catalog/2ead926aa93135bf69f858d47bdc08d1c1e80e8f/.github/UFGD.jpg",
+	},
+	{
+		university: "UNIVERSIDADE FEDERAL DA INTEGRAÇÃO LATINOAMERICANA",
+		state: "Paraná",
+		img: "https://raw.githubusercontent.com/jairo-sousa/boracodar-7-catalog/2ead926aa93135bf69f858d47bdc08d1c1e80e8f/.github/UNILA.jpg",
+	},
+	{
+		university: "UNIVERSIDADE FEDERAL DA PARAÍBA",
+		state: "Paraíba",
+		img: "https://raw.githubusercontent.com/jairo-sousa/boracodar-7-catalog/2ead926aa93135bf69f858d47bdc08d1c1e80e8f/.github/UFPB.jpeg",
+	},
+	{
+		university: "UNIVERSIDADE FEDERAL DE ALAGOAS",
+		state: "Alagoas",
+		img: "https://raw.githubusercontent.com/jairo-sousa/boracodar-7-catalog/2ead926aa93135bf69f858d47bdc08d1c1e80e8f/.github/UFAL.jpg",
+	},
+	{
+		university: "UNIVERSIDADE FEDERAL DE ALFENAS",
+		state: "Minas Gerais",
+		img: "https://raw.githubusercontent.com/jairo-sousa/boracodar-7-catalog/2ead926aa93135bf69f858d47bdc08d1c1e80e8f/.github/UNIFAL.jpg",
+	},
+	{
+		university: "UNIVERSIDADE FEDERAL DE CAMPINA GRANDE",
+		state: "Paraíba",
+		img: "https://raw.githubusercontent.com/jairo-sousa/boracodar-7-catalog/2ead926aa93135bf69f858d47bdc08d1c1e80e8f/.github/UFCG.webp",
+	},
+	{
+		university: "UNIVERSIDADE FEDERAL DE CIÊNCIAS DA SAÚDE DE PORTO ALEGRE",
+		state: "Rio Grande do Sul",
+		img: "https://raw.githubusercontent.com/jairo-sousa/boracodar-7-catalog/2ead926aa93135bf69f858d47bdc08d1c1e80e8f/.github/UFCSPA.jpg",
+	},
+];
